ab6577b2a696031c29f787664c82b042e1dac598
Steven Enamakel <enamakel@maha.xyz>
2025-01-12 01:31:25 +0400

    fixed build issues

--- next.config.ts
+++ next.config.ts
@@ -1,13 +1,14 @@
 import { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
-  experimental: {
-    optimizePackageImports: ["@chakra-ui/react"]
-  },
-  reactStrictMode: true,
+  // experimental: {
+  //   optimizePackageImports: ['@chakra-ui/react'],
+  // },
+  // reactStrictMode: true,
   eslint: {
     dirs: ['src'],
   },
+  output: 'export',
 };
 
 export default nextConfig;
